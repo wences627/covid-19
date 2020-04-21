ffc6811745b09805a95ecd1273805ec746f0c5e3
wences627 <wences628@gmail.com>
2020-04-21 02:57:12 -0400

    Updates

---
+++ precache-manifest.86cec7cf3c0a84702b777553fee9d441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c042ab538be930b688dc213a2df488b8",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "fc1a5d5e74d92f955cb5",
+    "url": "/covid-19/static/css/main.bc3b3473.chunk.css"
+  },
+  {
+    "revision": "10f8126dc958c2888091",
+    "url": "/covid-19/static/js/2.7e71e03a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.7e71e03a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1a5d5e74d92f955cb5",
+    "url": "/covid-19/static/js/main.9f76edc6.chunk.js"
+  },
+  {
+    "revision": "af2456817dc4e9d3858c",
+    "url": "/covid-19/static/js/runtime-main.956ba84f.js"
+  }
+]);
